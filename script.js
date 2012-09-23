a1117a671c66eb2b4da660271a13a6113d74d491
Alla Lipina <alla.lipina@gmail.com>
2012-09-23 10:43:53 +0400

    better tabs and JS

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 $(document).ready(function(){
+
 	$('.b-article, .b-article__title').hide();
-	$('.b-menu-horiz__item-link').on('click', function() {
-	
-	if (!$($(this).attr('href')).is(':visible')) {
+
+	$('.b-menu-horiz__item-link').click((function(){
+
+		if (!$($(this).attr('href')).is(':visible')){
+
+			basicMode();
+
+			$($(this).attr('href')).fadeIn(1000);
+			$(this).addClass('b-menu-horiz__item-link_selected');
+
+			return false;
+
+			} else {
+				basicMode();
+			}
+		});
+
+	$('body').dblclick(function(){
+		basicMode();
+	});
+
+	function basicMode(){
 		$('.b-article').hide();
 		$('.b-menu-horiz__item-link').removeClass('b-menu-horiz__item-link_selected');
-		$($(this).attr('href')).fadeIn(1000);
-		$(this).addClass('b-menu-horiz__item-link_selected');
-		return false;
-		}
-		else {
-			$('.b-article').hide();
-			$('.b-menu-horiz__item-link').removeClass('b-menu-horiz__item-link_selected');
-		}		
-	});
-			
-	$('body').on('dblclick', function(){
-		$('.b-article').hide();
-		$('.b-menu-horiz__item-link').removeClass('b-menu-horiz__item-link_selected');
-	});
+	}
 });
